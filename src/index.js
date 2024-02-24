f9b05b335827e137219a74ef07b1370fac96ab6f
DESKTOP-24V2TD5\Martiny <mawinClone@gmail.com>
2024-02-24 15:39:34 +0700

    myport saturday

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import About from './pages/about/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
