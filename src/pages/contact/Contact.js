3bb4597e63bcb1ecc5646d2d7cbbedd64a531f39
Mawin Nattawat <108991698+mawinClone@users.noreply.github.com>
2024-05-24 19:36:06 +0700

    Change hide contract info

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,8 +4,10 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="Contact">
-      <p>Tel: 080-8141308</p>
-      <p>Email: mawinLEGN@gmail.com</p>
+      {/* <p>Tel: 080-8141308</p> */}
+      <p>Tel: -</p>
+      {/* <p>Email: mawinLEGN@gmail.com</p> */}
+      <p>Email: -</p>
       <p>Line: mawin_98</p>
     </div>
   );
